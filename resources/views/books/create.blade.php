872e6b8a6cb0752270fb24d4918f97e84195d25c
Amit Yadav <email.ay@proton.me>
2024-06-27 18:02:07 +0530

    heading updated

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Add Books') }}
+            {{ __('Add New Book') }}
         </h2>
     </x-slot>
 
