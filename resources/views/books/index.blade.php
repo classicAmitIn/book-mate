4ed5b837f9d52d6278ee4fdcfa800268b509da37
Amit Yadav <email.ay@proton.me>
2024-06-27 16:19:58 +0530

    added edit and delete button

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -18,7 +18,39 @@
                                 <div>
                                     <span class="text-gray-800">{{ $book->user->name }}</span>
                                     <small class="ml-2 text-sm text-gray-600">{{ $book->created_at->format('j M Y, g:i a') }}</small>
+
+                                    @unless ($book->created_at->eq($book->updated_at))
+                                        <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
+                                    @endunless
+
                                 </div>
+
+                                @if ($book->user->is(auth()->user()))
+                                <x-dropdown>
+                                    <x-slot name="trigger">
+                                        <button>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                            </svg>
+                                        </button>
+                                    </x-slot>
+                                    <x-slot name="content">
+                                        <x-dropdown-link :href="route('books.edit', $book)">
+                                            {{ __('Edit') }}
+                                        </x-dropdown-link>
+
+                                        <form method="POST" action="{{ route('books.destroy', $book) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <x-dropdown-link :href="route('books.destroy', $book)" onclick="event.preventDefault(); this.closest('form').submit();">
+                                                {{ __('Delete') }}
+                                            </x-dropdown-link>
+                                        </form>
+
+                                    </x-slot>
+                                </x-dropdown>
+                            @endif
+
                             </div>
                             <p class="mt-4 text-lg text-gray-900">{{ $book->title }}</p>
                         </div>
